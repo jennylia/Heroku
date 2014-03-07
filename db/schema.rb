6ea90d389f935ce8d3002230d1a34dd30e2e75cc
Leo Teng Liu <liutenghou@gmail.com>
2014-03-06 23:15:07 -0800

    with leo branch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140307004719) do
+ActiveRecord::Schema.define(version: 20140307070055) do
+
+  create_table "clinics", force: true do |t|
+    t.string   "SV_TAXONOMY"
+    t.string   "TAXONOMY_NAME"
+    t.string   "RG_REFERENCE"
+    t.string   "RG_NAME"
+    t.string   "SV_REFERENCE"
+    t.string   "SV_NAME"
+    t.string   "SV_DESCRIPTION"
+    t.string   "SL_REFERENCE"
+    t.string   "LC_REFERENCE"
+    t.string   "PHONE_NUMBER"
+    t.string   "WEBSITE"
+    t.string   "EMAIL_ADDRESS"
+    t.string   "WHEELCHAIR_ACCESSIBLE"
+    t.string   "LANGUAGE"
+    t.string   "HOURS"
+    t.string   "STREET_NUMBER"
+    t.string   "STREET_NAME"
+    t.string   "STREET_TYPE"
+    t.string   "STREET_DIRECTION"
+    t.string   "CITY"
+    t.string   "PROVINCE"
+    t.string   "POSTAL_CODE"
+    t.integer  "LATITUDE"
+    t.integer  "LONGITUDE"
+    t.string   "811_LINK"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "locations", force: true do |t|
     t.string   "name"
